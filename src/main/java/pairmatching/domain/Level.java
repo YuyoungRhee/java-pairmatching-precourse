b278a4e68955f4e0132c24fd806838b71ca6774f
YuyoungRhee <bbclara@naver.com>
2024-12-05 18:36:56 +0900

    feat(Level): 레벨에 해당하는 미션을 검증하는 기능 구현

--- src/main/java/pairmatching/domain/Level.java
+++ src/main/java/pairmatching/domain/Level.java
@@ -1,17 +1,47 @@
 package pairmatching.domain;
 
+import static pairmatching.common.error.ErrorMessage.NOT_FOUND_LEVEL;
+import static pairmatching.common.error.ErrorMessage.NOT_FOUND_MISSION;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum Level {
-    LEVEL1("레벨1"),
-    LEVEL2("레벨2"),
-    LEVEL3("레벨3"),
-    LEVEL4("레벨4"),
-    LEVEL5("레벨5");
+    LEVEL1("레벨1", Arrays.asList("자동차경주", "로또", "숫자야구게임")),
+    LEVEL2("레벨2", Arrays.asList("장바구니", "결제", "지하철노선도")),
+    LEVEL3("레벨3", Collections.emptyList()),
+    LEVEL4("레벨4", Arrays.asList("성능개선", "배포")),
+    LEVEL5("레벨5", Collections.emptyList());
 
-    private String name;
+    private final String name;
+    private final List<String> mission;
 
-    Level(String name) {
+    Level(String name, List<String> mission) {
         this.name = name;
+        this.mission = mission;
     }
 
-    // 추가 기능 구현
+    public static Level from(String input) {
+        for (Level level : Level.values()) {
+            if (level.name.equals(input)) {
+                return level;
+            }
+        }
+        throw new IllegalArgumentException(NOT_FOUND_LEVEL.getMessage());
+    }
+
+    public static void validateMissionName(Level level, String mission) {
+        List<String> missions = level.getMission();
+        boolean existInLevel = missions.stream()
+                .anyMatch(s -> s.equals(mission));
+
+        if (!existInLevel) {
+            throw new IllegalArgumentException(NOT_FOUND_MISSION.getMessage());
+        }
+    }
+
+    public List<String> getMission() {
+        return mission;
+    }
 }
---
+++ src/test/java/pairmatching/domain/LevelTest.java
@@ -0,0 +1,21 @@
+package pairmatching.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static pairmatching.common.error.ErrorMessage.NOT_FOUND_MISSION;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LevelTest {
+
+    @DisplayName("해당 레벨에 존재하지 않는 미션인 경우 예외를 발생시킨다.")
+    @Test
+    void validateMissionName() {
+        assertThatThrownBy(() -> {
+            Level.validateMissionName(Level.LEVEL1, "오징어게임");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(NOT_FOUND_MISSION.getMessage());
+
+    }
+
+}
